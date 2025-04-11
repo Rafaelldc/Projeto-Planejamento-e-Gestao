bf95cfef3d6aa20d00982c212f7d93f3bdbcb32e
Rafael Lopes de Carvalho <rafaelldc2027@gmail.com>
2025-04-11 09:26:03 -0300

    Atualização

---
+++ app-swing/src/main/java/br/com/aproxcrmsolutions/AppSwingApplication.java
@@ -0,0 +1,13 @@
+package br.com.aproxcrmsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSwingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSwingApplication.class, args);
+	}
+
+}
---
+++ app-swing/src/test/java/br/com/aproxcrmsolutions/AppSwingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aproxcrmsolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSwingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
